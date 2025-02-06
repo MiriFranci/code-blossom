5801281a5b95e91dffc52d06787d9f642c8acfd8
MiriFranci <francipanem@Gmail.com>
2025-02-06 16:43:27 +0100

    commit iniziale

---
+++ src/main/java/com/example/code_blossom/CodeBlossomApplication.java
@@ -0,0 +1,13 @@
+package com.example.code_blossom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeBlossomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeBlossomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/code_blossom/CodeBlossomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.code_blossom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeBlossomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
